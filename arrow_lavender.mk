466faf15a3cea940dba8a040ac5f502cf14a4c2d
ELbee <85691313+woodyfi@users.noreply.github.com>
2025-01-04 21:39:46 +0700

    Update and rename afterlife_lavender.mk to arrow_lavender.mk

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -14,17 +14,13 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_WIFI_EXT := true
-AFTERLIFE_BUILD_TYPE := OFFICIAL
-AFTERLIFE_MAINTAINER := sweetbtrfly
-AFTERLIFE_GAPPS := true
-AFTERLIFE_CORE := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := afterlife_lavender
+PRODUCT_NAME := arrow_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
