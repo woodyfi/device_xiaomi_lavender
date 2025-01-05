804c0b3dc6f662f14d68db97003092e55863578e
ELbee <85691313+woodyfi@users.noreply.github.com>
2025-01-05 11:38:50 +0700

    Update arrow_lavender.mk

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common afterlife stuff
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+# Inherit some common arrow stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
